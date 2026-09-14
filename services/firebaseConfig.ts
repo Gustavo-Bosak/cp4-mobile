efdec12cfc5b1e04fa395b6c0b82b3669f1ee28c
gustavo-bosak <gustavobosaksantos@gmail.com>
2026-09-14 20:36:10 -0300

    :card_file_box: config(firebase): adiciona dados sobre app do firebase

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -1,20 +1,24 @@
 import { initializeApp, FirebaseApp } from "firebase/app";
 import { initializeAuth, Auth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const {getReactNativePersistence} = require("firebase/auth") as any;
 
 // Ative "Authentication" > "Sign-in method" > "E-mail/senha" no seu projeto.
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID",
+  apiKey: "AIzaSyB124pvDRHNKqortDCt-gSea03BSfZWNX8",
+  authDomain: "cp4-mobile-28f1d.firebaseapp.com",
+  projectId: "cp4-mobile-28f1d",
+  storageBucket: "cp4-mobile-28f1d.firebasestorage.app",
+  messagingSenderId: "9476054478",
+  appId: "1:9476054478:web:820190e7fbda4d1aaadf9f",
+  measurementId: "G-C8X07WM1EE"
 };
 
 const app: FirebaseApp = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
 
 export const auth: Auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
